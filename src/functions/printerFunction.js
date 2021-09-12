84134c65929333e28c2d6d8f2a457fe0d0d10adc
AhmedDridi-code <ahmeddridi.1234@gmail.com>
2021-09-12 06:55:10 +0200

    new changes in architecture and EJS

--- src/functions/printerFunction.js
+++ src/functions/printerFunction.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const { render } = require("../../app");
 
 const Printer = require("../models/printer");
 
@@ -12,7 +13,10 @@ exports.show = (req, res) => {
         return res.status(500).json({ message: error.message });
     })
 };
+exports.newPrinter = (req, res) => {
+    res.render('addPrinter');
 
+};
 exports.add = (req, res) => {
     const printer = new Printer({
         _id: new mongoose.Types.ObjectId(),
@@ -22,7 +26,7 @@ exports.add = (req, res) => {
         paper: req.body.paper,
     });
     printer.save().then((result) => {
-        return res.status(200).json(result);
+        res.redirect('/printer/show',{printers:printers});
     }).catch((error) => {
         return res.status(error.code).json({ error: error.message });
     });
--- src/routes/printerRoutes.js
+++ src/routes/printerRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {add, edit, remove, show } = require('../functions/printerFunction');
+const { add, edit, remove, show, newPrinter } = require('../functions/printerFunction');
 
 router.get('/show', show);
 router.post('/add', add);
+router.get('/new', newPrinter);
 router.patch('/edit', edit);
 router.delete('/remove', remove);
 
