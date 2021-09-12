4679c668b99612977164e90d92566bc10461d265
AhmedDridi-code <ahmeddridi.1234@gmail.com>
2021-09-12 18:57:53 +0200

    consommable and printer Modules finished

--- src/functions/consommableFunction.js
+++ src/functions/consommableFunction.js
@@ -1,55 +1,60 @@
-
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const consommable = require("../models/consommable");
 
 const Consommable = require("../models/consommable");
 
 exports.show = (req, res) => {
-    Consommable.find({}).then((result)=>{
-        return res.status(200).json(result);
-    }).catch((error)=>{
-        return res.status(500).json({message:error.message});
+    Consommable.find({}).then((consommables) => {
+        res.render("Consommable", { consommables: consommables });
+    }).catch((error) => {
+        return res.status(500).json({ message: error.message });
     })
 };
-
+exports.addForm = (req, res) => {
+    res.render("addConsommable");
+}
+exports.editForm = (req, res) => {
+    res.render("editConsommable", { id: req.params.id });
+}
 exports.add = (req, res) => {
     const consommable = new Consommable({
-    _id: new mongoose.Types.ObjectId(),
-    articleNumber: req.body.articNumber,
-    type : req.body.type,
-    colorType:req.body.colorType,
-    pageSum: req.body.pageSum,
-    percent: req.body.percent,
-    message: req.body.message,
-});
-consommable.save().then((result)=>{
-    return res.status(200).json(result);
-}).catch((error)=>{
-   return res.status(error.code).json({error:error.message});
-});
+        _id: new mongoose.Types.ObjectId(),
+        articleNumber: req.body.articleNumber,
+        type: req.body.type,
+        colorType: req.body.colorType,
+        pageSum: Number(req.body.pageSum),
+        percent: req.body.percent,
+        message: req.body.message,
+    });
+    consommable.save().then((result) => {
+        res.redirect('/consommable/show');
+    }).catch((error) => {
+        return res.status(500).json({ error: error.message });
+    });
 };
 
 exports.edit = (req, res) => {
-    Consommable.findByIdAndUpdate(req.body.id,{
-    articleNumber: req.body.articNumber,
-    type : req.body.type,
-    colorType:req.body.colorType,
-    pageSum: req.body.pageSum,
-    percent: req.body.percent,
-    message: req.body.message,
-}).then((result)=>{
-    return res.status(200).json(result);
-}).catch((error)=>{
-    return res.status(error.code).json({error:error.message});
-});
+    Consommable.findByIdAndUpdate(req.params.id, {
+        articleNumber: req.body.articleNumber,
+        type: req.body.type,
+        colorType: req.body.colorType,
+        pageSum: req.body.pageSum,
+        percent: req.body.percent,
+        message: req.body.message,
+    }).then((result) => {
+        res.redirect('/consommable/show');
+    }).catch((error) => {
+        return res.status(error.code).json({ error: error.message });
+    });
 
 };
 
 exports.remove = (req, res) => {
-    Consommable.findByIdAndRemove(req.body.id).then((result)=>{
-        return res.status(200).json(result);
-    }).catch((error)=>{
-        return res.status(error.code).json({error:error.message});
+    Consommable.findByIdAndRemove(req.params.id).then((result) => {
+        res.redirect('/consommable/show');
+    }).catch((error) => {
+        return res.status(error.code).json({ error: error.message });
     })
 };
--- src/routes/consommableRoutes.js
+++ src/routes/consommableRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const {add, edit, remove, show } = require('../functions/consommableFunction');
+const { add, edit, remove, show, addForm, editForm } = require('../functions/consommableFunction');
 
 router.get('/show', show);
 router.post('/add', add);
-router.patch('/edit', edit);
-router.delete('/remove', remove);
+router.patch('/edit/:id', edit);
+router.delete('/remove/:id', remove);
+router.get('/new', addForm);
+router.get('/edit/:id', editForm);
 
 module.exports = router;
